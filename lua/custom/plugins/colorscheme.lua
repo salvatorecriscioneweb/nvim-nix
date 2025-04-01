252d6d35b266354ea031e8a7f72821d51770316c
Salvatore Criscione <salvatore.n@grrlz.net>
2025-04-01 23:50:17 +0300

    updated colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,12 +1,32 @@
 return {
   {
-    'nyoom-engineering/oxocarbon.nvim',
+    'Mofiqul/vscode.nvim',
     version = false,
     lazy = false,
     priority = 1000,
     config = function()
-      vim.opt.background = 'dark' -- set this to dark or light
-      vim.cmd.colorscheme 'oxocarbon'
+      local c = require('vscode.colors').get_colors()
+      require('vscode').setup {
+        -- Alternatively set style in setup
+        -- style = 'light'
+
+        -- Enable transparent background
+        transparent = true,
+
+        -- Enable italic comment
+        italic_comments = true,
+
+        -- Underline `@markup.link.*` variants
+        underline_links = true,
+
+        -- Disable nvim-tree background color
+        disable_nvimtree_bg = true,
+
+        -- Apply theme colors to terminal
+        terminal_colors = true,
+      }
+      vim.opt.background = 'light' -- set this to dark or light
+      vim.cmd.colorscheme 'vscode'
     end,
   },
 }
