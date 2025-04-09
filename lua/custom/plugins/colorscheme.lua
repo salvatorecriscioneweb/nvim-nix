130ad424c3bc87a2b0a9f6b02d731590fb4a35be
Salvatore Criscione <salvatore.n@grrlz.net>
2025-04-09 11:01:56 +0300

    update colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -17,6 +17,7 @@ return {
       vim.cmd.colorscheme 'fogbell_light'
       vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#d0d0d0' })
       vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = '#262626', fg = '#d0d0d0' })
+      vim.api.nvim_set_hl(0, 'MsgArea', { bg = '#262626', fg = '#d0d0d0' })
     end,
   },
 }
