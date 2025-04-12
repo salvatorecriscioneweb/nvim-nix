6f2d75d789715a1b7978568e951ab7f8d7438d78
Salvatore Criscione <salvatore.n@grrlz.net>
2025-04-13 00:43:43 +0300

    flake update

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,23 +1,34 @@
 return {
-  -- {
-  --   'slugbyte/lackluster.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   init = function()
-  --     vim.cmd.colorscheme 'lackluster'
-  --     -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
-  --     -- vim.cmd.colorscheme("lackluster-mint")
-  --   end,
-  -- },
   {
-    'jaredgorski/fogbell.vim',
+    'slugbyte/lackluster.nvim',
     lazy = false,
-    config = function()
-      vim.opt.background = 'light'
-      vim.cmd.colorscheme 'fogbell_light'
-      vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#d0d0d0' })
-      vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = '#262626', fg = '#d0d0d0' })
-      vim.api.nvim_set_hl(0, 'MsgArea', { bg = '#262626', fg = '#d0d0d0' })
+    priority = 1000,
+    init = function()
+      -- vim.cmd.colorscheme 'lackluster'
+      vim.cmd.colorscheme 'lackluster-hack' -- my favorite
+      -- vim.cmd.colorscheme("lackluster-mint")
     end,
   },
+  -- {
+  --   'bluz71/vim-moonfly-colors',
+  --   name = 'moonfly',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'moonfly'
+  --     -- Lua initialization file
+  --     vim.g.moonflyTransparent = true
+  --   end,
+  -- },
+  -- {
+  --   'jaredgorski/fogbell.vim',
+  --   lazy = false,
+  --   config = function()
+  --     vim.opt.background = 'light'
+  --     vim.cmd.colorscheme 'fogbell_light'
+  --     vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#d0d0d0' })
+  --     vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = '#262626', fg = '#d0d0d0' })
+  --     vim.api.nvim_set_hl(0, 'MsgArea', { bg = '#262626', fg = '#d0d0d0' })
+  --   end,
+  -- },
 }
